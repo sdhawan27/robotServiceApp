bac5dd85df65240538f9167cd45cbcbdadd44175
sdhawan27 <53635699+sdhawan27@users.noreply.github.com>
2020-03-01 14:24:55 +0530

    adding consul
    Adding consul

--- src/main/java/com/xebia/robotServiceApp/RobotServiceAppApplication.java
+++ src/main/java/com/xebia/robotServiceApp/RobotServiceAppApplication.java
@@ -2,8 +2,10 @@ package com.xebia.robotServiceApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RobotServiceAppApplication {
 
 	public static void main(String[] args) {
